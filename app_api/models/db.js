f7a11c656508a17e28b4334f9fb89a57f2697d13
AtakanTurgut <atakan.trgt@hotmail.com>
2022-12-02 20:55:15 +0300

    odev7

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 require("./mekansema");
-var dbURI = "mongodb://localhost/mekanbul";
-//var dbURI= "mongodb+srv://atakan:1234@mekanbul.3u2lk3r.mongodb.net/mekanbul?retryWrites=true&w=majority";
+//var dbURI = "mongodb://localhost/mekanbul";
+var dbURI= "mongodb+srv://atakan:1234@mekanbul.3u2lk3r.mongodb.net/mekanbul?retryWrites=true&w=majority";
 mongoose.connect(dbURI);
 
 function kapat(msg, callback) {
--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,76 +1,96 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
-const anaSayfa=function(req, res, next) {
-    res.render('anasayfa', 
-    { 
-        "baslik" : 'Anasayfa',
+var apiSecenekleri = {
+    //sunucu: "http://localhost:3000",
+    sunucu:"https://mekanbul.atakanturgut.repl.co",
+    apiYolu: "/api/mekanlar/",
+};
+
+var mesafeyiFormatla = function(mesafe) {
+    var yeniMesafe, birim;
+    if (mesafe > 1) {
+        yeniMesafe = parseFloat(mesafe).toFixed(1);
+        birim = " km";
+    } else {
+        yeniMesafe = parseInt(mesafe * 1000, 10);
+        birim = " m"
+    }
+    return yeniMesafe + birim;
+};
+
+var anaSayfaOlustur = function(res, mekanListesi) {
+    var mesaj;
+    if (!(mekanListesi instanceof Array)) {
+        mesaj = "API HATASI: Birşeyler ters gitti!";
+        mekanListesi = [];
+    } else {
+        if (!mekanListesi.length) {
+            mesaj = "Civarda herhangi bir mekan bulunamadı!";
+        }
+    }
+    res.render("anasayfa", {
+        "baslik" : "Anasayfa",
         "sayfaBaslik" : {
-            "siteAd" : "MekanBul",
+            "siteAd" : "Mekanbul",
             "slogan" : "Civardaki Mekanları Keşfet!"
         },
-        "mekanlar" : [
-            {
-                "ad" : "Starbucks",
-                "puan" : "3",
-                "adres" : "Centrum Garden AVM",
-                "imkanlar" : ["Dünya Kahveleri", "Kekler", "Pastalar"],
-                "mesafe" : "10km"
-            },
-            {
-                "ad" : "Barida Kafe",
-                "puan" : "4",
-                "adres" : "Sdü Batı Kampüsü",
-                "imkanlar" : ["Kahve", "Çay", "Kek"],
-                "mesafe" : "8km"
-            }
-        ]
+        "mekanlar" : mekanListesi,
+        "mesaj" : mesaj
     });
 };
 
-const mekanBilgisi=function(req, res, next) {
-    res.render('mekanbilgisi', 
-    { 
-        "baslik" : 'Mekan Bilgisi',
-        "mekanBaslik" : "Starbucks",
-        "mekanDetay" : {
-            "ad" : "Starbucks",
-            "puan" : "3",
-            "adres" : "Centrum Garden AVM",
-            "saatler" : [
-                {
-                    "gunler":"Pazartesi-Cuma",
-                    "acilis":"9:00",
-                    "kapanis":"23:00",
-                    "kapali": false
-                },
-                {
-                    "gunler":"Cumartesi-Pazar",
-                    "acilis":"10:00",
-                    "kapanis":"22:00",
-                    "kapali": false
-                }
-            ],
-            "imkanlar" : ["Dünya Kahveleri", "Kekler", "Pastalar"],
-            "koordinatlar" : {
-                "enlem" : "37.7",
-                "boylam" : "30.5"
-            },
-            "yorumlar" : [
-                {
-                    "yorumYapan" : "Atakan Turgut",
-                    "yorumMetni" : "Harika!!!",
-                    "tarih" : "20 Ekim 2022",
-                    "puan" : "4"
-                },
-                {
-                    "yorumYapan" : "Asım Sinan Yüksel",
-                    "yorumMetni" : "Kahveler iyi.",
-                    "tarih" : "14 Ekim 2022",
-                    "puan" : "3"
-                },
-            ]
+const anaSayfa = function(req, res, next) {
+    axios.get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu, {
+        params : {
+            enlem : req.query.enlem,
+            boylam : req.query.boylam
+        },
+    }).then(function(response) {
+        var i, mekanlar;
+        mekanlar = response.data;
+        for (i = 0; i < mekanlar.length; i++) {
+            mekanlar[i].mesafe = mesafeyiFormatla(mekanlar[i].mesafe);
         }
+        anaSayfaOlustur(res, mekanlar);
+    }).catch(function(hata) {
+        anaSayfaOlustur(res, hata);
+    });
+};
+
+var detaySayfasiOlustur = function(res, mekanDetaylari) {
+    mekanDetaylari.koordinat = {
+        "enlem" : mekanDetaylari.koordinat[0],
+        "boylam" : mekanDetaylari.koordinat[1]
+    }
+    res.render('mekanbilgisi', {
+        mekanBaslik: mekanDetaylari.ad,
+        mekanDetay: mekanDetaylari
+    });
+};
+
+var hataGoster = function(res, hata) {
+    var mesaj;
+    if (hata.response.status == 404) {
+        mesaj = "404, Sayfa bulunamadı!";
+    } else {
+        mesaj = hata.response.status + " hatası.";
+    }
+    res.status(hata.response.status);
+    res.render('error', {
+        "mesaj" : mesaj
+    });
+};
+
+const mekanBilgisi = function(req, res) {
+    axios
+    .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
+    .then(function(response) {
+        detaySayfasiOlustur(res, response.data);
+    })
+    .catch(function(hata) {
+        hataGoster(res, hata);
     });
 };
 
